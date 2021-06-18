ae75a7deb5bfdf47f2fdcceb82b3eaf1f651e630
Antonio Gonzalez <antonio.gonzalez@accedo.tv>
2021-06-18 21:52:55 +0200

    Implement logic to manage overridden URLs in the App to redirect to the proper target

--- app/src/main/java/tv/accedo/itvappsflyerpoc/MainActivity.java
+++ app/src/main/java/tv/accedo/itvappsflyerpoc/MainActivity.java
@@ -12,20 +12,19 @@ import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.Toast;
 
-import org.json.JSONObject;
-
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
 public class MainActivity extends AppCompatActivity {
     private static final String LOG_TAG = MainActivity.class.getSimpleName();
+    public static MainActivity activity;
+    WebView myWebView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        WebView myWebView = (WebView) findViewById(R.id.webview);
+        activity = this;
+
+        myWebView = (WebView) findViewById(R.id.webview);
         myWebView.setWebViewClient(new MyWebViewClient());
         WebSettings webSettings = myWebView.getSettings();
         webSettings.setJavaScriptEnabled(true);
@@ -37,33 +36,72 @@ public class MainActivity extends AppCompatActivity {
 
     public static final String MARKET_AMAZON_URL = "amzn://apps/android?asin=";
     public static final String WEB_AMAZON_URL = "http://www.amazon.com/gp/mas/dl/android?asin=";
+    public static final String ANDROID_LINK_SCHEMA = "itvapp:";
+    // britbox asin = "B084RCQNF6";
+    // iTV asin = "B00PH7XGQG";
+    // SHA256: 0C:B9:10:EA:0C:4B:7B:8F:B8:50:96:EB:B5:02:49:DF:08:20:38:AF:AA:B8:78:07:5B:FD:4F:E8:1C:30:FD:0A
 
-    private static void openOnAmazonMarket(Context context, String packageName) {
-
+    private static void openOnAmazonMarket(Context context, String asin) {
         try {
-            Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(MARKET_AMAZON_URL + packageName));
+            Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(MARKET_AMAZON_URL + asin));
             intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
             context.startActivity(intent);
         } catch (android.content.ActivityNotFoundException anfe) {
-            Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(WEB_AMAZON_URL + packageName));
+            Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(WEB_AMAZON_URL + asin));
             intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
             context.startActivity(intent);
         }
-
     }
 
     private class MyWebViewClient extends WebViewClient {
-        @Override
         public boolean shouldOverrideUrlLoading(WebView view, String url) {
             Log.d(LOG_TAG, "Initial URL: " + url);
             Uri link = Uri.parse(url);
             Context mContext = getApplicationContext();
-
             String host = "";
             String asin = "";
 
+            // At startup, let "onelink" URL to be processed as a regular URL to allow redirection
+            if (url.contains("onelink.me")) {
+                Log.d(LOG_TAG, "Open a OneLink URL: " + url);
+                return false;
+            }
+
+            // URL returned by AppsFlyer contains "intent://".
+            // Therefore, proper redirection url must be extracted to be used afterwards.
+            if (url.contains("intent://")) {
+                // af_dp example: itvapp://mainactivity
+                // af_android_custom_url example: amzn://apps/android?asin=B00PH7XGQG
+
+                // af_dp represents the Android Link to open an App
+                String af_dp = link.getQueryParameter("af_dp");
+                int androidLinkIndex = af_dp.lastIndexOf("/");
+                String appId = af_dp.substring(androidLinkIndex + 1);
+                Intent launchIntent = getPackageManager().getLaunchIntentForPackage(appId);
+
+                // App is installed, set the URL to open it
+                if (launchIntent != null) {
+                    url = af_dp;
+                    Log.d(LOG_TAG, "URL obtained from Intent URL af_id param: " + url);
+                }
+                // App is not installed, set the URL to open the App Store
+                else {
+                    // af_android_custom_url represents the Android Link to open the App Store
+                    url = link.getQueryParameter("af_android_custom_url");
+                    Log.d(LOG_TAG, "URL obtained from Intent URL af_android_custom_url param: " + url);
+                }
+            }
+
+            // Check if the new Android URL belongs to the app currently running
+            if (url.contains(ANDROID_LINK_SCHEMA)) {
+                Log.d(LOG_TAG, "Package already running. Reload...");
+                Toast.makeText(mContext, "Reload running webapp", Toast.LENGTH_SHORT).show();
+                WebView myWebView = (WebView) findViewById(R.id.webview);
+                myWebView.loadUrl("file:///android_asset/index.html");
+                return false;
+            }
             // Google Play Logic
-            if ("play.google.com".equals(link.getHost())) {
+            else if (url.contains("play.google.com")) {
                 if (!url.startsWith("http://") && !url.startsWith("https://")) {
                     link = Uri.parse("http://" + url);
                 }
@@ -91,58 +129,19 @@ public class MainActivity extends AppCompatActivity {
 
                     openOnAmazonMarket(mContext, asin);
 
-                    //String britboxAsin = "B084RCQNF6";
-                    //String iTVAsin = "B00PH7XGQG";
-
                     return true;
                 }
             }
-            /*else if (url.contains("app.tv.accedo")) {
-                String[] appUrl = link.toString().replace("https://app.", "").split("/|\\?");
-                String host = appUrl[0];
-                String asin = appUrl.length > 1 ? appUrl[1] : "";
-                Log.d(LOG_TAG, "Try to use Referral Link: " + host + "/" + asin);
+            // DeepLink to App Logic
+            else if (url.contains("app://")) {
+                Log.d(LOG_TAG, "Open an Android URL: " + url);
 
-                Pattern pattern = Pattern.compile("S.browser_fallback_url=(.*?);", Pattern.DOTALL);
-                Matcher matcher = pattern.matcher(link.toString());
-                while (matcher.find()) {
-                    appUrl = matcher.group(1).replace("https://app.", "").split("/");
-                    host = appUrl[0];
-                    asin = appUrl.length > 1 ? appUrl[1] : "";
-                    Log.d(LOG_TAG, "Try to use Firebase Link: " + host + "/" + asin);
-                }
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                mContext.startActivity(intent);
 
-                Intent launchIntent = getPackageManager().getLaunchIntentForPackage(host);
-                if (launchIntent != null) {
-                    launchIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                    Log.d(LOG_TAG, "Open installed package: " + launchIntent.getPackage());
-                    String msg = host + " " + getString(R.string.apps_to_handle_intent);
-                    Toast.makeText(mContext, msg, Toast.LENGTH_SHORT).show();
-
-                    startActivity(launchIntent);
-
-                    if (launchIntent.getPackage().equals(getApplicationContext().getPackageName())) {
-                        Log.d(LOG_TAG, "Package already running...");
-                        Toast.makeText(mContext, "Reload running webapp", Toast.LENGTH_SHORT).show();
-                        WebView myWebView = (WebView) findViewById(R.id.webview);
-                        myWebView.loadUrl("javascript:window.location.reload( true )");
-                    }
-
-                } else if (asin.length() > 0) {
-                    Log.d(LOG_TAG, "Cannot open package: " + host);
-                    String msg = host + " " + getString(R.string.no_apps_to_handle_intent);
-                    Toast.makeText(mContext, msg, Toast.LENGTH_SHORT).show();
-
-                    openOnAmazonMarket(mContext, asin);
-
-                    //String britboxAsin = "B084RCQNF6";
-                    //String iTVAsin = "B00PH7XGQG";
-                } else {
-                    String msg = getString(R.string.no_apps_found);
-                    Toast.makeText(mContext, msg, Toast.LENGTH_SHORT).show();
-                }
                 return true;
-            }*/
+            }
 
             // No App nor Google play link, treat it as a regular URL
             Log.d(LOG_TAG, "Open a regular URL: " + url);
